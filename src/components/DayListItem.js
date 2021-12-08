b36d75a567ad8caffd466953e2030d611e07ab4d
Ivan <ivanxintang@gmail.com>
2021-12-08 14:30:35 -0800

    Refactor props to not pass id and pass in a function with it instead

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,9 +2,9 @@ import React from "react";
 import classNames from "classnames";
 import "./DayListItem.scss"
 export default function DayListItem(props) {
-  const { id, name, spots, selected, setDay } = props;
+  const { name, spots, selected, setDay } = props;
 
-  const dayListItemClass = classNames("day-list__item", {
+  const classList = classNames("day-list__item", {
     "day-list__item--selected": selected,
     "day-list__item--full": spots === 0
   });
@@ -15,7 +15,7 @@ export default function DayListItem(props) {
   }
 
   return (
-    <li key={id} className={dayListItemClass} onClick={() => setDay(name)}>
+    <li className={classList} onClick={() => setDay(name)}>
       <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
