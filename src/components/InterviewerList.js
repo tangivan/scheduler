67d60da19322d21c64da19c5752c75dbb803fa7a
Ivan <ivanxintang@gmail.com>
2021-12-08 14:33:07 -0800

    Implement InterviewerList and the styles for it

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import InterviewerListItem from './InterviewerListItem';
+import "./InterviewerList.scss";
+
+const InterviewerList = (props) => {
+  const { interviewers, onChange, value } = props;
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewers.map(interviewerItem =>
+          <InterviewerListItem
+            key={interviewerItem.id}
+            name={interviewerItem.name}
+            avatar={interviewerItem.avatar}
+            selected={interviewerItem.id === value}
+            setInterviewer={() => onChange(interviewerItem.id)}
+          />)}
+      </ul>
+    </section>
+  );
+}
+
+export default InterviewerList;
