f261dcb993a5b69b81cffc1395d7349ad1c952c8
Ivan <ivanxintang@gmail.com>
2021-12-10 13:43:29 -0800

    Implement getInterviewers

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,14 @@
-export function getAppointmentsForDay(state, day) {
+export default function getAppointmentsForDay(state, day) {
   const findDay = state.days.find(dayThing => dayThing.name === day);
   if (!findDay) return [];
   const appointmentsForDay = findDay.appointments.map(item => state.appointments[item]);
   return appointmentsForDay;
+}
+
+export function getInterview(state, interview) {
+  if (!interview) return null;
+  return {
+    ...interview,
+    interviewer: state.interviewers[interview.interviewer]
+  };
 }
