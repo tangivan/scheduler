a214e645582e045fc279cc2b9b9b3c9a592af7fb
Ivan <ivanxintang@gmail.com>
2021-12-09 16:56:26 -0800

    Add stories for Form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
+import Empty from './Empty';
+import Header from './Header';
+import Show from './Show';
 import "./styles.scss";
 
-const Appointment = ({ time }) => {
+const Appointment = ({ time, interview }) => {
   return (
     <article className="appointment">
-      {time ? `Appointment at ${time}` : 'No Appointments'}
+      {/* {time ? `Appointment at ${time}` : 'No Appointments'} */}
+      {<Header time={time} />}
+      {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
     </article>
   );
 }
