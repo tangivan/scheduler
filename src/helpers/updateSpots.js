71d38032cc9d1043bd2723ea14ee47fe41143efb
Ivan <ivanxintang@gmail.com>
2021-12-15 21:34:24 -0800

    Add clarifying comments

--- src/helpers/updateSpots.js
+++ src/helpers/updateSpots.js
@@ -1,10 +1,13 @@
 import getAppointmentsForDay from "./selectors";
 
 const updateSpots = (state, appointments) => {
+  //Filters for appointments with null interviews
   const spots = getAppointmentsForDay(
     { ...state, appointments },
     state.day
   ).filter((appoints) => !appoints.interview).length;
+
+  //Update specific day in the days state
   const days = state.days.map((day) =>
     day.name === state.day ? { ...day, spots } : day
   );
