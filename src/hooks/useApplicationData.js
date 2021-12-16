b35f82174611ac567c9bb46822aa0f4821d06d65
Ivan <ivanxintang@gmail.com>
2021-12-15 19:17:30 -0800

    Remove state logic and put into reducer, add web sockets

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,91 +1,73 @@
-import { useState, useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import axios from "axios";
+import applicationReducer, {
+  SET_DAY,
+  SET_INTERVIEW,
+  SET_APPLICATION_DATA,
+} from "reducers/application";
 
 const useApplicationData = () => {
-  const [state, setState] = useState({
+  const [state, dispatch] = useReducer(applicationReducer, {
     day: "Monday",
     days: [],
     appointments: {},
     interviewers: {},
   });
 
-  const setDay = (day) => setState({ ...state, day });
+  const setDay = (day) => dispatch({ type: SET_DAY, day });
+
+  const bookInterview = (id, interview) => {
+    const endpoint = `/api/appointments/${id}`;
+    return axios.put(endpoint, { interview }).then(() => {
+      dispatch({ type: SET_INTERVIEW, id, interview });
+    });
+  };
+
+  const deleteInterview = (id) => {
+    const endpoint = `/api/appointments/${id}`;
+    return axios
+      .delete(endpoint)
+      .then(() => dispatch({ type: SET_INTERVIEW, id, interview: null }));
+  };
 
   useEffect(() => {
-    const daysURL = "http://localhost:8001/api/days";
-    const appointmentsURL = "http://localhost:8001/api/appointments";
-    const interviewersURL = "http://localhost:8001/api/interviewers";
+    const webSocket = new WebSocket(
+      process.env.REACT_APP_WEBSOCKET_URL,
+      "protocolOne"
+    );
+
+    webSocket.onopen = () => {
+      webSocket.send("ping");
+    };
+
+    webSocket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      if (message.type === SET_INTERVIEW) {
+        dispatch(message);
+      }
+    };
+    return () => webSocket.close();
+  }, []);
+
+  useEffect(() => {
+    const daysURL = "/api/days";
+    const appointmentsURL = "/api/appointments";
+    const interviewersURL = "/api/interviewers";
 
     Promise.all([
       axios.get(daysURL),
       axios.get(appointmentsURL),
       axios.get(interviewersURL),
     ]).then((all) => {
-      setState((prev) => ({
-        ...prev,
-        days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data,
-      }));
+      const days = all[0].data;
+      const appointments = all[1].data;
+      const interviewers = all[2].data;
+      dispatch({
+        type: SET_APPLICATION_DATA,
+        data: { days, appointments, interviewers },
+      });
     });
-  }, []);
-
-  const bookInterview = (id, interview) => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview },
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-    const endpoint = `/api/appointments/${id}`;
-    const days = updateSpots();
-    return axios
-      .put(endpoint, appointment)
-      .then(() =>
-        setState((prevState) => ({ ...prevState, appointments, days }))
-      );
-  };
-
-  const deleteInterview = (id) => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null,
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-    const endpoint = `/api/appointments/${id}`;
-    const days = updateSpots("delete");
-    return axios
-      .delete(endpoint, appointment)
-      .then(() =>
-        setState((prevState) => ({ ...prevState, appointments, days }))
-      );
-  };
-
-  const updateSpots = (type = "save") => {
-    //Get the day object
-    const day = { ...state.days.find((day) => day.name === state.day) };
-    //Map all appointments for that day, filter only null appointments
-    const nullAppointments = day.appointments
-      .map((element) => state.appointments[element])
-      .filter((appoints) => !appoints.interview);
-    //manipulate spots based on type of update (save/delete)
-    const offset = type === "save" ? -1 : 1;
-    //create new day
-    const newDay = {
-      ...day,
-      spots: nullAppointments.length + offset,
-    };
-    //map out new days state
-    const newDays = state.days.map((day) =>
-      day.id === newDay.id ? newDay : day
-    );
-    return newDays;
-  };
+  }, [dispatch]);
 
   return { state, setDay, bookInterview, deleteInterview };
 };
